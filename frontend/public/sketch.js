88ee2f123884a558192c3dfe893e0b38b863b405
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-10-29 00:04:40 +0100

    Start with p5.js

---
+++ frontend/public/sketch.js
@@ -0,0 +1,14 @@
+/** Setup */
+function setup() {
+	createCanvas( 800, 450 );
+	frameRate(20);
+}
+
+/** Draws frame on canvas. */
+function draw() {
+	// Sky
+	background( 120, 200, 250 );
+
+	// Platform
+	rect( 0, 400, 800, 50 )
+}
