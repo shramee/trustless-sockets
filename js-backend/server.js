a7e7a52e53044e338cd3aabfcd3aafb61a910ed1
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-10-31 02:19:04 +0000

    Update server.js

--- js-backend/server.js
+++ js-backend/server.js
@@ -2,15 +2,91 @@ import { Server } from "socket.io";
 
 const io = new Server(5000, { cors: { origin: "*" } });
 
-const players = [];
+let players = [];
+
 
 io.on("connection", (socket, data) => {
   // send a message to the client
-  socket.emit( 'players_data', {} );
-  socket.emit( 'game_state', {} );
 
   // receive a message from the client
-  socket.on( 'player', (...args) => {
-    console.log("Client says...", args);
+  socket.on( 'player', ( player ) => {
+    players.push( [player, socket] );
+//    if ( players.length > 1 ) {
+      new GameOn( players );
+      players = [];
+//    }
   });
 });
+
+
+class GameOn {
+  players;
+  gameState = {
+    playerPos: [],
+  };
+  gameEvents = [];
+  pendingEvents = [];
+
+  gameRange = {
+    x: 800,
+    y: 450,
+  }
+
+  constructor( players ) {
+    this.players = players;
+    this.players.forEach( ([player, socket], id) => {
+      this.listen( id, socket )
+      this.initPlayerState( id );
+    } );
+
+    this.emit( 'players_data', players.map( ([player]) => player ) );
+    this.emit( 'game_state', this.gameState );
+  }
+
+  initPlayerState( id ) {
+    this.pendingEvents[id] = [];
+    this.gameEvents[id] = [];
+    this.gameState.playerPos[id] = [
+      Math.floor( 64 + Math.random() * (this.gameRange.x - 128) ),
+      Math.floor( 64 + Math.random() * (this.gameRange.y - 128) ),
+    ]
+  }
+
+  emit( ev, msg ) {
+    if ( typeof ev === 'string' ) {
+      ev = [[ev, msg]];
+    }
+    this.players.forEach( ([_, socket]) => {
+        ev.forEach( ([event, msg]) => {
+          socket.emit( event, msg )
+        } );
+    } );
+  }
+
+  /**
+   * Adds events to pending events array
+   * @param id
+   * @param socket
+   */
+  listen( id, socket ) {
+    socket.on( 'input', input => {
+      if ( typeof input === 'string' || ! input.length ) {
+        this.pendingEvents[id].push( input );
+      } else {
+        this.pendingEvents[id].push( ...input );
+      }
+    } );
+  }
+
+  prepareState( input ) {
+    const oldState = this.gameState;
+  }
+
+  start() {
+//    this.emit( 'game_state', {} );
+  }
+
+  processState() {
+
+  }
+}
